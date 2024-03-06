a94a0fd2060b1168cfb667245e0a03189e407feb
RomainCrgnl <159545165+RomainCrgnl@users.noreply.github.com>
2024-03-06 12:01:44 +0100

    Update Visu_temporelle_donnees.py

--- Visu_temporelle_donnees.py
+++ Visu_temporelle_donnees.py
@@ -12,36 +12,44 @@ ending_year = 2004
 
 # Paramètres de l'animation
 frames = (ending_year+1-starting_year)*12  # Nombre de trames dans l'animation
-sigma = 0.2  # Paramètre de lissage pour le noyau gaussien
+sigma = 0.1  # Paramètre de lissage pour le noyau gaussien
 
 # Mise en place du tracé
-fig, ax = plt.subplots()
+fig, (ax1, ax2) = plt.subplots(2,1)
 sns.set(style="white", palette="muted", color_codes=True)
-ax.set_xlim(0,10)
-ax.set_ylim(-7,7)   
-ax.set_xlabel('Pd')
-ax.set_ylabel('Bz') 
 
 # Création de toutes les dates de l'animation
 range_mois = ['01','02','03','04','05','06','07','08','09','10','11','12']
 dates = [str(annee)+'-'+mois for annee in range(starting_year,ending_year+1) for mois in range_mois]
 
 def update(frame):   
+    ax1.clear()
+    ax1.set_xlim(0,10)
+    ax1.set_ylim(-7,7)   
+    ax1.set_xlabel('Pd')
+    ax1.set_ylabel('Bz') 
+    
+    ax2.clear()
+    ax2.set_xlim(5,18)
+    ax2.set_ylim(0,1)
+    
     date = dates[frame]
     
     data = df[(df.index.strftime('%Y-%m-%d %H:%M:%S')).str.startswith(date)]
 
+    ax2.hist(data["r0_shue_1998"],density=True,bins=30)
+    
     # Ploter les points de données
-    sns.scatterplot(x=data["Pd"],y=data["Bz"], ax=ax, color='b', alpha=0.5)
+    sns.scatterplot(x=data["Pd"],y=data["Bz"], ax=ax1, color='b', alpha=0.5)
     
     # Calculer la densité des points avec un noyau gaussien
-    sns.kdeplot(x=data["Pd"],y=data["Bz"], cmap="Blues", fill=True, ax=ax, levels=15, thresh=0, bw_method=sigma)
+    sns.kdeplot(x=data["Pd"],y=data["Bz"], cmap="Blues", fill=True, ax=ax1, levels=15, thresh=0, bw_method=sigma)
     
     # Ajouter des informations supplémentaires si nécessaire
-    ax.set_title(dates[frame])
+    ax1.set_title(dates[frame])
 
 # Créer l'animation
-animation = FuncAnimation(fig, update, frames=frames)
+animation = FuncAnimation(fig, update, frames=frames,interval=100)
 
 # Afficher l'animation
 plt.show()
